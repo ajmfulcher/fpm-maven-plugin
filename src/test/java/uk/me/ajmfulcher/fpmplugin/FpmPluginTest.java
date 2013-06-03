b8ee639993113c7360f6c3eeb3dd442d6649c42e
Andrew Fulcher <andrew.fulcher@gmail.com>
2013-06-03 13:20:25 +0100

    Removed the typeArg parameter; this is now derived from inputType and packageName

--- src/test/java/uk/me/ajmfulcher/fpmplugin/FpmPluginTest.java
+++ src/test/java/uk/me/ajmfulcher/fpmplugin/FpmPluginTest.java
@@ -12,9 +12,10 @@ public class FpmPluginTest {
 		String inputType = "gem";
 		String outputType = "deb";
 		String outputDir = "/tmp";
-		String typeArg = "json";
-		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,typeArg);
-		String[] args = { "-s","gem","-t","deb","-C","/tmp/fpm/base","--workdir","/tmp/fpm/tmp","json" };
+		String packageName = "json";
+		String packageVersion = "1.8.0";
+		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,packageName, packageVersion);
+		String[] args = { "-s","gem","-t","deb","-C","/tmp/fpm/base","--workdir","/tmp/fpm/tmp","-v","1.8.0","json" };
 		assertArrayEquals(args,fpm.getArgs());
 	}
 	
@@ -23,21 +24,23 @@ public class FpmPluginTest {
 		String inputType = "gem";
 		String outputType = "deb";
 		String outputDir = "/tmp";
-		String typeArg = "json";
-		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,typeArg);
+		String packageName = "json";
+		String packageVersion = "1.8.0";
+		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,packageName, packageVersion);
 		String optionalArgs = "--debug";
-		String[] args = { optionalArgs, "-s","gem","-t","deb","-C","/tmp/fpm/base","--workdir","/tmp/fpm/tmp","json" };
+		String[] args = { optionalArgs, "-s","gem","-t","deb","-C","/tmp/fpm/base","--workdir","/tmp/fpm/tmp","-v","1.8.0","json" };
 		fpm.setOptionalArgs(optionalArgs);
 		assertArrayEquals(args,fpm.getArgs());
 	}
 	
 	@Test
-	public void createPackage(){
+	public void createGemPackage(){
 		String inputType = "gem";
 		String outputType = "deb";
 		String outputDir = targetDir().toString();
-		String typeArg = "json";
-		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,typeArg);
+		String packageName = "json";
+		String packageVersion = "1.8.0";
+		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,packageName, packageVersion);
 		try {
 			fpm.invoke();
 		} catch (Exception e) {
@@ -45,6 +48,75 @@ public class FpmPluginTest {
 		}
 		File file = new File(outputDir + "/fpm/rubygem-json_1.8.0_amd64.deb");
 		assertTrue(file.exists());
+	}
+	
+	@Test
+	public void createDirPackage(){
+		String inputType = "dir";
+		String outputType = "deb";
+		String outputDir = targetDir().toString();
+		String packageName = "stuff";
+		String packageVersion = "1.2.3";
+		
+		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,packageName,packageVersion);
+		
+		// Create some files to test against
+		File testUsrDir = new File(outputDir + "/fpm/base/usr");
+		File testEtcDir = new File(outputDir + "/fpm/base/etc");
+		testUsrDir.mkdir();
+		testEtcDir.mkdir();
+		
+		try {
+			fpm.invoke();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		File file = new File(outputDir + "/fpm/stuff_1.2.3_amd64.deb");
+		assertTrue(file.exists());
+		
+		// Teardown
+		testUsrDir.delete();
+		testEtcDir.delete();
+	}
+	
+	@Test
+	public void getTypeArgsForDir(){
+		String inputType = "dir";
+		String outputType = "deb";
+		String outputDir = targetDir().toString();
+		String packageName = "json"; // This should be deprecated later on
+		String packageVersion = "1.8.0";
+		
+		// Create some files to test against
+		File testUsrDir = new File(targetDir() + "/fpm/base/usr");
+		File testEtcDir = new File(targetDir() + "/fpm/base/etc");
+		testUsrDir.mkdir();
+		testEtcDir.mkdir();
+		
+		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,packageName, packageVersion);
+
+		// And the response we expect
+		String[] response = { "etc", "usr" };
+		assertArrayEquals(fpm.getTypeArgs(inputType),response);
+		
+		// Teardown
+		testUsrDir.delete();
+		testEtcDir.delete();
+	}
+	
+	@Test
+	public void getTypeArgsForOther(){
+		String inputType = "gem";
+		String outputType = "deb";
+		String outputDir = targetDir().toString();
+		String packageName = "json"; // This should be deprecated later on
+		
+		// And the response we expect
+		String[] response = { "json" };
+		
+		String packageVersion = "1.8.0";
+		FpmPlugin fpm = new FpmPlugin(inputType,outputType,outputDir,packageName, packageVersion);
+		assertArrayEquals(fpm.getTypeArgs(inputType),response);
 	}
 	
 	public File targetDir(){
